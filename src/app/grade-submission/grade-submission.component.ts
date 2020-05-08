ca951b64dc38b54a3fc085c8fb00da17e17463d1
Stephen Shiao <ss2sc@virginia.edu>
2020-05-07 23:13:12 -0400

    auto update fix

--- src/app/grade-submission/grade-submission.component.ts
+++ src/app/grade-submission/grade-submission.component.ts
@@ -15,7 +15,6 @@ export class GradeSubmissionComponent implements OnInit {
 
   @ViewChild('editor', {static: false}) editor;
   files: any[] = [{filename: "File 1", content: "Placeholder Text"}];
-  currFile: Number = 0;
 
   title: string = AssignmentData.title;
   description: string = AssignmentData.description;
@@ -38,27 +37,26 @@ export class GradeSubmissionComponent implements OnInit {
 
   ngAfterViewInit() {
     this.editor.setTheme("eclipse");
-    this.editor.text = this.files[0].content;
   }
 
   loadFiles(event: any) {
     this.files = [];
     let file = event.target.files[0];
-    let numFiles = event.target.files.length;
     let fileReader = new FileReader();
 
     if(file.name.endsWith(".zip")) {
-      fileReader.onload = (fileLoadedEvent: Event) => {
+      fileReader.onload = () => {
         let zip = new JSZip();
         zip.loadAsync(file).then((zip) => {
           Object.keys(zip.files).forEach((filename) => {
             zip.files[filename].async('text').then((fileData) => {
               this.files.push({filename: filename, content: fileData});
+            })
           })
         })
-      })
-    }} else {
-      fileReader.onload = (fileLoadedEvent: Event) => {
+      } 
+    } else {
+      fileReader.onload = () => {
         let text = <String>fileReader.result;
         this.files.push({filename: file.name, content: text});
       }
